174854b78965b6adfc6b1fcfe85c132ea8b44569
Jefferson Anaya <jefryerson062@gmail.com>
2023-10-11 21:52:34 -0500

    a

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import FirstApp from './challenge1/FirstApp.jsx'
+import Handle from './challenge2/Handle.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FirstApp />
+    <Handle />
   </React.StrictMode>,
 )
