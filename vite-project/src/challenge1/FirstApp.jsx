3e7c19ac9e237ec0865bbd33e3a4267c2f522fa7
Jefferson Anaya <jefryerson062@gmail.com>
2023-10-11 21:49:31 -0500

    a

---
+++ vite-project/src/challenge1/FirstApp.jsx
@@ -0,0 +1,14 @@
+const FirstApp = () => {
+    return (
+        <>
+            <h1>
+                My First Aplication
+            </h1>
+            <span>
+                10
+            </span>
+        </>
+    )
+}
+export default FirstApp
+
---
+++ vite-project/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import FirstApp from './challenge1/FirstApp.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FirstApp />
+  </React.StrictMode>,
+)
